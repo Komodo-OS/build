413c16f0dcbcbcdbe81596e152d8a03f50043457
goodmeow <harunbam3@gmail.com>
2020-06-19 22:42:32 +0700

    build: KomodoQuickStep

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -23,6 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 # /product packages
 PRODUCT_PACKAGES += \
     Camera2 \
+    KomodoQuickStep \
     Contacts \
     DeskClock \
     Gallery2 \
