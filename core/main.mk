sai4041412/android_build

build: Add our props to ADDITIONAL_BUILD_PROPERTIES
Change-Id: I81f55f3a0a91e79eca4dcd22cf8f10354fff7b30

core/main.mk
@@ -175,6 +175,10 @@ else
   endif
 endif
 
+#
+# -----------------------------------------------------------------
+# Add our DU properties to the build properties.
+ADDITIONAL_BUILD_PROPERTIES += $(DU_PROPERTIES)
 
 # Bring in standard build system definitions.
 include $(BUILD_SYSTEM)/definitions.mk

Komodo-OS/build

Signed-off-by: alanndz <alanmahmud0@gmail.com>

core/main.mk
@@ -154,6 +154,10 @@ else
   endif
 endif
 
+#
+# -----------------------------------------------------------------
+# Add our KOMODO properties to the build properties.
+ADDITIONAL_BUILD_PROPERTIES += $(KOMODO_PROPERTIES)
 
 # Bring in standard build system definitions.
 include $(BUILD_SYSTEM)/definitions.mk