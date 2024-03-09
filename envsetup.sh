1e3b1485be54f0c575679750a54d9b37866411fd
Ido Ben-Hur <idoybh2@gmail.com>
2024-03-09 19:18:53 +0200

    make: envsetup: Always use latest release for lunch
    lets us keep the old lunch format
    also fixes bugs with lunch and roomservice for A14/QPR2
    
    [someone5678]
    * Add old lunch format while keeping new format
    
    Original commit:
    https://github.com/yaap/build_make/commit/058f83d4e9751e2612e1bc9447f5ba1898e1a88d
    
    Change-Id: Ic8307fb8690c3328871f97ec9ce1aeb1c8dc6036
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- envsetup.sh
+++ envsetup.sh
@@ -485,7 +485,14 @@ function lunch()
     # This must be <product>-<release>-<variant>
     local product release variant
     # Split string on the '-' character.
-    IFS="-" read -r product release variant <<< "$selection"
+    if [[ $(echo $selection | grep -o "-" | wc -l) = 1 ]];
+    then
+        # Always pick the latest release
+        release=$(grep "BUILD_ID" build/make/core/build_id.mk | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 1 | tr '[:upper:]' '[:lower:]')
+        IFS="-" read -r product variant <<< "$selection"
+    else
+        IFS="-" read -r product release variant <<< "$selection"
+    fi
 
     if [[ -z "$product" ]] || [[ -z "$release" ]] || [[ -z "$variant" ]]
     then
