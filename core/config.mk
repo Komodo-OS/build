5c9e7eb6ea1aa86dc4ba75789e20862c0f8a5497
wulan17 <wulan17@nusantararom.org>
2025-03-28 19:27:59 +0700

    build: Use own fork of device sepolicy
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- core/config.mk
+++ core/config.mk
@@ -1293,10 +1293,10 @@ include $(BUILD_SYSTEM)/sysprop_config.mk
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
 ifneq ($(KOMODO_BUILD),)
-ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
+ifneq ($(wildcard device/komodo/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+$(eval include device/komodo/sepolicy/common/sepolicy.mk)
 endif
 endif
 
